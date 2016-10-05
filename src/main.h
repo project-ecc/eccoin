3a604de4e01a71c0a687d6a49913ef52e552bf05
Greg Griffith <tfa.productions.1337@gmail.com>
2016-10-05 02:04:09 -0700

    Revert "raised min tx fee"
    This reverts commit b1d465dbaee1206723e0fc263ba38bfc3236f0b2.

--- src/main.h
+++ src/main.h
@@ -51,7 +51,7 @@ static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
 static const unsigned int MAX_INV_SZ = 50000;
 
 extern int64_t nBestTimeReceived;
-static const int64_t MIN_TX_FEE = 1;
+static const int64_t MIN_TX_FEE = 0.1;
 static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
 static const int64_t MAX_MONEY = 50000000000 * COIN;
 static const int64_t COIN_YEAR_REWARD = 10 * CENT; // 10% per year
