1db8067f910e503ed1c746857ee048657f782bef
Greg Griffith <tfa.productions.1337@gmail.com>
2016-10-13 19:56:43 -0700

    allow older versions to connect
    allow older versions to connect

--- src/version.h
+++ src/version.h
@@ -33,7 +33,7 @@ static const int DATABASE_VERSION = 70508;
 static const int PROTOCOL_VERSION = 60027;
 
 // earlier versions not supported as of Feb 2012, and are disconnected
-static const int MIN_PROTO_VERSION = 60026;
+static const int MIN_PROTO_VERSION = 60021;
 
 // nTime field added to CAddress, starting with this version;
 // if possible, avoid requesting addresses nodes older than this
