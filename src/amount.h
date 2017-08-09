9eaa8b02fbe3cafe6cef79a502d16315e3f62a63
Greg Griffith <cryptounitedteam@gmail.com>
2017-08-08 20:49:57 -0700

    fix amounts

--- src/amount.h
+++ src/amount.h
@@ -13,8 +13,8 @@
 
 typedef int64_t CAmount;
 
-static const CAmount COIN = 100000000;
-static const CAmount CENT = 1000000;
+static const CAmount COIN = 1000000;
+static const CAmount CENT = 10000;
 
 extern const std::string CURRENCY_UNIT;
 
@@ -27,7 +27,7 @@ extern const std::string CURRENCY_UNIT;
  * critical; in unusual circumstances like a(nother) overflow bug that allowed
  * for the creation of coins out of thin air modification could lead to a fork.
  * */
-static const CAmount MAX_MONEY = 21000000 * COIN;
+static const CAmount MAX_MONEY = 50000000000 * COIN;
 inline bool MoneyRange(const CAmount& nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
 
 /** Type-safe wrapper class for fee rates
