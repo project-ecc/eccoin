33a958cc4530029a2c9e06d3a00132b05c941342
Greg Griffith <tfa.productions.1337@gmail.com>
2017-09-09 01:27:11 -0400

    use pos limit for pos not pow limit
    use pos limit for pos not pow limit

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -190,7 +190,7 @@ bool CheckStakeKernelHash(int nHeight, unsigned int nBits, const CBlock& blockFr
         bool fNegative;
         bool fOverflow;
         hashTarget.SetCompact(GetNextTargetRequired(chainActive.Tip(), true), &fNegative, &fOverflow);
-        if (fNegative || hashTarget == 0 || fOverflow || hashTarget > UintToArith256(Params().GetConsensus().powLimit))
+        if (fNegative || hashTarget == 0 || fOverflow || hashTarget > UintToArith256(Params().GetConsensus().posLimit))
             return error("CheckStakeKernelHash(): nBits below minimum work");
         /// the older the coins are, the higher the day weight. this means with a higher dayWeight you get a bigger reduction in your hashProofOfStake
         /// this should lead to older and older coins needing to be selected as the difficulty rises due to fast block minting. larger inputs will also help this
