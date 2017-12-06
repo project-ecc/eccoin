6678c474c5d32b88853f95827ecbe263f4eca6ff
greg-griffith <griffith@cryptounited.io>
2017-12-06 20:22:51 +0000

    fix source of network name

--- src/eccoind.cpp
+++ src/eccoind.cpp
@@ -109,7 +109,7 @@ bool AppInit(int argc, char* argv[])
         }
         // Check for -testnet or -regtest parameter (Params() calls are only valid after this clause)
         try {
-            CheckParams(network);
+            CheckParams(ChainNameFromCommandLine());
         } catch (const std::exception& e) {
             fprintf(stderr, "Error: %s\n", e.what());
             return false;
