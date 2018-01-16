648127d208ccc56493219d0775ce811f8923e6ad
Greg Griffith <griffith@cryptounited.io>
2018-01-16 01:02:09 -0500

    add missing copyright to ans files
    add missing copyright to ans files

--- src/ans/ansrecord.cpp
+++ src/ans/ansrecord.cpp
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 Greg Griffith and the ECC developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #include "ansrecord.h"
 
 void CAnsRecord::setValue(std::string strValue)
