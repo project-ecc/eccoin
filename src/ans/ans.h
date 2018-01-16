42472b32cf7f34e7ac49fbbc43ec4eaa4dc04275
Greg Griffith <griffith@cryptounited.io>
2018-01-16 01:02:09 -0500

    add missing copyright to ans files
    add missing copyright to ans files

--- src/ans/ans.h
+++ src/ans/ans.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2018 Greg Griffith and the ECC developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef ANS_H
 #define ANS_H
 
