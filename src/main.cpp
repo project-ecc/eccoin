9f167ff8a9371807524352e5ec2f0e79dfb78dda
Greg Griffith <griffith@cryptounited.io>
2018-04-08 23:41:15 -0400

    raise max fee for service tx to 50 * COIN

--- src/main.cpp
+++ src/main.cpp
@@ -681,7 +681,7 @@ bool AcceptToMemoryPoolWorker(CTxMemPool& pool, CValidationState &state, const C
                 strprintf("%d > %d", nFees, ::minRelayTxFee.GetFee(nSize) * 10000));
         }
 
-        if (fRejectAbsurdFee && tx.nVersion == 2 && nFees > ::minRelayTxFee.GetFee(nSize) * 5000000)
+        if (fRejectAbsurdFee && tx.nVersion == 2 && nFees > ::minRelayTxFee.GetFee(nSize) * 50000000)
         {
             LogPrintf("Absurdly-high-fee of %d for tx with version of 2 \n", nFees);
             return state.Invalid(false, REJECT_HIGHFEE, "absurdly-high-fee",
