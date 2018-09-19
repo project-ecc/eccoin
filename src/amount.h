59cd30ad2cb9aaaf33c1fc68c9fed677a65e4b85
Greg Griffith <griffith@cryptounited.io>
2018-09-19 15:45:45 -0700

    reduce amount of satoshis in a coin from 100M to 1M

--- src/amount.h
+++ src/amount.h
@@ -28,8 +28,8 @@
 
 typedef int64_t CAmount;
 
-static const CAmount COIN = 100000000;
-static const CAmount CENT = 1000000;
+static const CAmount COIN = 1000000;
+static const CAmount CENT = 10000;
 
 extern const std::string CURRENCY_UNIT;
 
