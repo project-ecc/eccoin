8af55b360759129381f78fddfc3d0549d1904ad2
Griffith <8345264+Greg-Griffith@users.noreply.github.com>
2019-08-06 12:23:31 -0700

    bump version to 0.2.5.17 (#235)

--- src/clientversion.h
+++ src/clientversion.h
@@ -21,7 +21,7 @@
 #define CLIENT_VERSION_MAJOR 0
 #define CLIENT_VERSION_MINOR 2
 #define CLIENT_VERSION_REVISION 5
-#define CLIENT_VERSION_BUILD 16
+#define CLIENT_VERSION_BUILD 17
 
 //! Set to true for release, false for prerelease or test build
 #define CLIENT_VERSION_IS_RELEASE true
